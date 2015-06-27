b2bb6b49333c25f1a6f57febbb0f9c10a735a146
adikrasimirowa <adi_krasimirowa@abv.bg>
2015-06-27 18:53:25 +0300

    Create login.php

---
+++ HM3/ex.3/login.php
@@ -0,0 +1,15 @@
+<html> 
+<head> <meta charset="UTF-8"></head>
+<body>
+<h3>е-Мания</h3>
+<form action="buy.php" method="post">
+Потребител:<input type="text" name="user">
+Парола:<input type="text" name="pass">
+	<input type="submit" name="send" value="Влез">
+</form>
+</body>
+</html>
+<?php
+session_start();
+
+?>
